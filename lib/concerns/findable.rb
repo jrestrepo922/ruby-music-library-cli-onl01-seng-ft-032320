1195248ad15df51a7697378dc5d42cd252ec8e9c
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-11 22:07:28 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,7 +2,7 @@
 
 module Concerns::Findable
   def find_by_name(name)
-    self.all.find{|song| song.name == song_name}
+    self.all.find{|song| song.name == name}
   end
 
   def find_or_create_by_name(name)
