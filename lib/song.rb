a1dccebaf18c0e146b5ec48954999ebce75ad631
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-11 22:39:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,7 +62,7 @@ class Song
     artist = Artist.find_or_create_by_name(artirst_name)
     genre = Genre.find_or_create_by_name(genre_name)
     self.new(song_name, artist, genre)
-    
+
     #binding.pry
 
   end
