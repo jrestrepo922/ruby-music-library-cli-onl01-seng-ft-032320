435cb2983760d90df63c8e53dd3841511da9fb34
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-11 17:37:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,7 +40,11 @@ class Artist
 
     else
     song.artist = self  # the song instance artist property was given an instace of the artist
-    self.songs=(song)    # calling the artist instance setter method to shovel the instance of a song into the @songs array
+      if songs.include?(song)
+
+      else
+        self.songs=(song)  # calling the artist instance setter method to shovel the instance of a song into the @songs array
+      end
     end
   end
 end
