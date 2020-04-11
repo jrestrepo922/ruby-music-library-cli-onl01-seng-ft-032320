bbd54fcb28f6d3bcdd645db3060b7f335a76c3c3
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-11 22:33:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,6 +58,7 @@ class Song
     song_name = filename.split(" - ")[1]
     artirst_name = filename.split(" - ")[0]
     new_song = self.new(song_name)
+    new_song.artist=(artirst_name)
     new_song
     binding.pry
 
