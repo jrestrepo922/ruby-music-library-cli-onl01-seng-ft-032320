3998eac4d0455b5380650eefeb455e680f442603
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-11 20:53:37 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -31,9 +31,9 @@ class Genre
     @songs
   end
 
-  def songs=(new_song) #setter method that shovels a song into the @songs array
-    songs << new_song
-  end
+  # def songs=(new_song) #setter method that shovels a song into the @songs array
+  #   songs << new_song
+  # end
 
   def artists
     @new_array_artist = []
