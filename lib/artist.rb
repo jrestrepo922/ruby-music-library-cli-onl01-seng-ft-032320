bde1ff9647941c97402b42c827521a55465261b3
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-11 21:24:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -36,21 +36,23 @@ class Artist
   # end
 
   def add_song(song)  #instance of a song
-    if song.artist
 
-    else
-    song.artist = self  # the song instance artist property was given an instace of the artist. The song knows it has an artist
-      if songs.include?(song)
-
-      else
-        self.songs << song  # calling the artist instance setter method to shovel the instance of a song into the @songs array. THe artist has added this song to a list of songs
-      end
-    end
+    song.artist = self ulsess song.arstist
+    # if song.artist
+    #   return
+    # else
+    # song.artist = self  # the song instance artist property was given an instace of the artist. The song knows it has an artist
+    #   if songs.include?(song)
+    #
+    #   else
+    #     self.songs << song  # calling the artist instance setter method to shovel the instance of a song into the @songs array. THe artist has added this song to a list of songs
+    #   end
+    # end
   end
 
   def genres
     #Genre.all
-    binding.pry
+
     @new_array = []
     Song.all.each {|song|   #songs intances have name, instances of artist and instance of genre
       if @new_array.include?(song.genre)
