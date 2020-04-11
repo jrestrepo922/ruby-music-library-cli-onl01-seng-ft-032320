2d4807b1f09afcc33ae44e369efa0a98f491d527
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-11 15:01:30 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create(name)
+    new_artist = self.new(name)  # creates an new instance of a song
+    new_artist.save # the new song is an intance and has an instance method of save witch shovel the intance into the Class variable
+    new_artist # returns the new song.
+  end
+end
