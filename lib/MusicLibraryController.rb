389ec22a191f4b791014838b712d2451dd6bb52a
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-12 00:49:32 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -38,7 +38,15 @@ class MusicLibraryController
 
 
   def list_artists
+    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
+      puts "#{i+1}. #{a.name}"
+    end
+  end
 
+  def list_genres
+    Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |g, i|
+      puts "#{i+1}. #{g.name}"
+    end
   end
 
 
